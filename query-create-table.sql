7349c6706a1f5ada4d0aac6173cad085884118a4
littleboy <littleboy@MacBook-Pro-de-Loic.local>
2023-11-02 10:52:41 +0100

    added comments & explanations

--- query-create-table.sql
+++
@@ -1,7 +0,0 @@
--- create a table with a :
--- id as uuid
--- name
-CREATE TABLE books (
-   id uuid,
-   name varchar(255)
-);
--- src/main.py
+++ src/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+import psycopg2
+import sys
+
+sys.stdout.flush()
+
+# We connect to the database (Postgres) with :
+# - psycopg2 package
+# - infos coming from the setup we did in docker-compose.yml
+# http://bdd:5432
+conn = psycopg2.connect(
+    host="bdd",
+    port="5432",
+    database="john",
+    user="john",
+    password="example"
+)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Here is defined a route to the book listing
+# We call it with (GET) http://localhost:8000/books
+@app.get("/books")
+def list_books():
+    # Simple & concise implementation of fetching "books" from database
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books')
+    books = cur.fetchall()
+    cur.close()
+
+    # We return books, without any kind of formatting.
+    return books
+
